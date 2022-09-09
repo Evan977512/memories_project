ba272ca3b0a9af1236fb7a462cc9d7fcd32887d8
EvanKim <youngchan.kim@edu.sait.ca>
2022-09-09 08:45:40 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,4 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import cars from "cars";
+
+const app = express();
+
+app.use(bodyParser.json({ linit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ linit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://emdwlekr:rladudcks91@cluster0.wuba1f8.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
