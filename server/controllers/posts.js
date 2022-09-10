85bfc3ba6198ab3d1758d2d1f3a32a1579ad99b5
EvanKim <youngchan.kim@edu.sait.ca>
2022-09-09 21:48:33 -0600

    controllers updated

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,6 +2,33 @@
 // hanels all logics on routes/posts.js
 // in order to keep the logic visible....
 
-export const getPosts = (req, res) => {
-  res.send("THIS WORKS!!!!!!");
+// import model
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    //return result
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.satus(404).json({ errorMessage: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  // create new PostMessage object and save it in newPost
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    // https://www.restapitutorial.com/httpstatuscodes.html
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ errorMessage: error.message });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 // import controllers/posts.js
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ const router = express.Router();
 
 // localhost:5000/posts
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
